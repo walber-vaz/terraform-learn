b03e6f1d3459eaf90780acccab02612904a5725e
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-12-06 09:27:12 -0300

    refactor: update security group rules for EC2 instance

--- modules/ec2/sg.tf
+++ modules/ec2/sg.tf
@@ -1,22 +1,37 @@
 resource "aws_security_group" "w2k_sg" {
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
+  ingress = [
+    {
+      from_port = 0
+      to_port   = 0
+      protocol  = "-1"
+      self      = true
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = var.ip_ssh_connection
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["::/0"]
+    },
+  ]
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.ip_ssh_connection
-  }
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-  }
+  egress = [
+    {
+      cidr_blocks = ["0.0.0.0/0"]
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+    }
+  ]
 }
