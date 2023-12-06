d7104c3010b07599785a04af3b49d03b8a6e8ddb
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-12-06 09:45:01 -0300

    refactor: add filter for virtualization type in data.tf

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -6,5 +6,10 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
   owners = ["099720109477"] # Canonical
 }
